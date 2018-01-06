ec93cf0a5fcce103534461adce18734b892b934d
Petar Petrov <pppepito86@gmail.com>
2018-01-06 03:59:29 +0200

    ensure set instance type

--- src/main/java/org/pesho/judge/rest/RestService.java
+++ src/main/java/org/pesho/judge/rest/RestService.java
@@ -2,6 +2,7 @@ package org.pesho.judge.rest;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.pesho.judge.Worker;
 import org.pesho.judge.WorkersQueue;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.services.ec2.model.InstanceType;
@@ -40,10 +42,12 @@ public class RestService {
 	}
 
 	@GetMapping("/workers/ensure/{count}")
-	public ResponseEntity<?> ensureWorkers(@PathVariable("count") int count) {
+	public ResponseEntity<?> ensureWorkers(@PathVariable("count") int count,
+			@RequestParam("type") Optional<String> maybeType) {
+		InstanceType type = InstanceType.valueOf(maybeType.orElse(InstanceType.T2Micro.toString()));
 		Configuration configuration = new Configuration()
 				.setImageId("ami-099a0966")
-				.setInstanceType(InstanceType.T2Micro)
+				.setInstanceType(type)
 				.setSecurityGroup("All")
 				.setNewInstanceKeyName("pesho")
 				.setWorkerRegistryEndpoint("http://localhost:8889/workers")
