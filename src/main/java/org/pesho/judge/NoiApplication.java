cca22345280d3638e9aeacb49dfadbe50effa0b3
Petar Petrov <pppepito86@gmail.com>
2018-01-18 22:38:38 +0200

    fix build

--- src/main/java/org/pesho/judge/NoiApplication.java
+++ src/main/java/org/pesho/judge/NoiApplication.java
@@ -14,10 +14,8 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 @EnableScheduling
 public class NoiApplication {
 	
-	@Bean
 	public ThymeleafViewResolver thymeleafViewResolver() {
 	    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-	    resolver.setTemplateEngine(templateEngine());
 	    resolver.setCharacterEncoding("UTF-8");
 	    return resolver;
 	}
