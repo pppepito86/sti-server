13c46cf4605086419bd812eec8ae1a66e181632c
Petar Petrov <pppepito86@gmail.com>
2018-02-16 22:58:36 +0200

    update ami

--- src/main/java/org/pesho/judge/rest/HtmlService.java
+++ src/main/java/org/pesho/judge/rest/HtmlService.java
@@ -330,7 +330,7 @@ public class HtmlService implements RunTerminateListener {
 		int automaticCount = Math.max((count - manualCount + 1) / 2, 0);
 		InstanceType type = InstanceType.valueOf("C4Large");
 		Configuration configuration = new Configuration()
-				.setImageId("ami-bbd0b1d4")
+				.setImageId("ami-a6d1b0c9")
 				.setInstanceType(type)
 				.setSecurityGroup("All")
 				.setSecurityKeyName("noi")
