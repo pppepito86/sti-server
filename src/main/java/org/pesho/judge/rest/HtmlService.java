4c24fb31aa3a4a70f139f29bcb18a4ee3c688545
Petar Petrov <pppepito86@gmail.com>
2018-10-21 02:02:08 +0300

    error on wrong file upload

--- src/main/java/org/pesho/judge/rest/HtmlService.java
+++ src/main/java/org/pesho/judge/rest/HtmlService.java
@@ -212,6 +212,10 @@ public class HtmlService implements RunTerminateListener {
 			@RequestParam("problemNumber") Integer problemNumber,
 			Model model) throws Exception {
 		
+		if (!file.getOriginalFilename().toLowerCase().endsWith(".cpp")) {
+			return "redirect:/user/error?msg=4";
+		}
+		
 		long submissionTime = System.currentTimeMillis();
 		
 		String city = "Sofia";
