b8718cbcb7614b665f6fb542e9a59209b1439aac
Petar Petrov <pppepito86@gmail.com>
2018-10-21 14:09:29 +0300

    disable submit button when appripriate

--- src/main/resources/static/dist/js/timer2.js
+++ src/main/resources/static/dist/js/timer2.js
@@ -9,13 +9,20 @@ function showTimeToSubmit() {
     if (distance <= 0) {
         document.getElementById("timetosubmit2").innerHTML = "";
         document.getElementById("timetosubmit3").innerHTML = "";
+        document.getElementById("submitcodebutton2").disabled = false;
+        document.getElementById("submitcodebutton3").disabled = false;
+
         return;
     }
     
     var seconds = Math.floor(distance / 1000) + 1;
-    var text = "след " + seconds + " секунд"
-    if (seconds != 1) text = text + "и"
+    var text = "след " + seconds + " секунд";
+    if (seconds != 1) text = text + "и";
+    else text = text + "а";
     // Output the result in an element with id="demo"
+    document.getElementById("submitcodebutton2").disabled = true;
+    document.getElementById("submitcodebutton3").disabled = true;
+
     document.getElementById("timetosubmit2").innerHTML =  text;
     document.getElementById("timetosubmit3").innerHTML =  text;
     
