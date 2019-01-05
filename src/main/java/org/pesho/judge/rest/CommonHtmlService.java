82c2901eaf2e2202c1b4cbf0afc20354022538ee
Petar Petrov <pppepito86@gmail.com>
2019-01-05 02:21:42 +0200

    move /grade path

--- src/main/java/org/pesho/judge/rest/CommonHtmlService.java
+++ src/main/java/org/pesho/judge/rest/CommonHtmlService.java
@@ -1,17 +1,25 @@
 package org.pesho.judge.rest;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class CommonHtmlService extends HtmlService {
@@ -56,6 +64,41 @@ public class CommonHtmlService extends HtmlService {
         }
     }
 
-    
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/grade")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public synchronized String addSubmission(@RequestPart("file") MultipartFile file, 
+			@RequestParam("city") String city, Model model)
+			throws Exception {
+		File zipFile = getFile("temp", city, city + ".zip");
+		zipFile.getParentFile().mkdirs();
+		
+		FileUtils.copyInputStreamToFile(file.getInputStream(), zipFile);
+		File zipFolder = getFile("temp", city, city);
+		unzip(zipFile, zipFolder);
+
+		List<File> listSourceFiles = listSourceFiles(zipFolder);
+		for (File sourceFile: listSourceFiles) {
+			String username = sourceFile.getParentFile().getName();
+			String contest = sourceFile.getParentFile().getParentFile().getName();
+			String problemName = sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf('.'));
+			String fileName = sourceFile.getName();
+			int submissionId = repository.addSubmission(city, username, contest, problemName, fileName);
+			if (submissionId != 0) {
+				File newFile = getFile("submissions", String.valueOf(submissionId), fileName);
+				newFile.getParentFile().mkdirs();
+				FileUtils.copyFile(sourceFile, newFile);
+			} else {
+				String details = String.format("%s_%s_%s_%s", city, contest, username, problemName);
+				repository.addLog("submission", "problem not found for " + details, "");
+			}
+		}
+		
+		FileUtils.deleteQuietly(zipFile);
+		FileUtils.deleteQuietly(zipFolder);
+		
+		return "redirect:/admin/submissions";
+	}
+
     
 }
