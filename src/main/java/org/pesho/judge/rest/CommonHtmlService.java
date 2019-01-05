28b743a55d7ad7ae067190a55ec29c44053c1ba1
Petar Petrov <pppepito86@gmail.com>
2019-01-05 02:04:53 +0200

    secure city submissions

--- src/main/java/org/pesho/judge/rest/CommonHtmlService.java
+++ src/main/java/org/pesho/judge/rest/CommonHtmlService.java
@@ -1,9 +1,17 @@
 package org.pesho.judge.rest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommonHtmlService extends HtmlService {
@@ -23,30 +31,30 @@ public class CommonHtmlService extends HtmlService {
     	return "/login";
     }
 
-//	@GetMapping("/home")
-//    public String table(@RequestParam(value = "city") String city, Model model) {
-//    	city = city.toLowerCase();
-//    	List<Map<String,Object>> submissions = repository.listCitySubmissions(city);
-//    	if (submissions.size() > 0) {
-//    		Map<String, Map<String, List<Map<String, Object>>>> result = new HashMap<>();
-//    		Map<String, List<Map<String, Object>>> contestsSubmissions = submissions.stream().collect(Collectors.groupingBy(s->s.get("contest").toString()));
-//    		for (String contest: contestsSubmissions.keySet()) {
-//    			List<Map<String, Object>> contestSubmissions = contestsSubmissions.get(contest);
-//        		Map<String, List<Map<String, Object>>> usersSubmissions = contestSubmissions.stream().collect(Collectors.groupingBy(s->s.get("username").toString()));
-//        		for (List<Map<String, Object>> list: usersSubmissions.values()) {
-//        			while (list.size() < 3) list.add(new HashMap<>());
-//        		}
-//        		result.put(contest, usersSubmissions);
-//    		}
-//    		
-//    		System.out.println(result);
-//    		model.addAttribute("contests", result);
-//        	return "result";
-//        } else {
-//        	model.addAttribute("city", city);
-//        	return "upload";
-//        }
-//    }
+    @PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/home")
+    public String table(@RequestParam(value = "city") String city, Model model) {
+    	city = city.toLowerCase();
+    	List<Map<String,Object>> submissions = repository.listCitySubmissions(city);
+    	if (submissions.size() > 0) {
+    		Map<String, Map<String, List<Map<String, Object>>>> result = new HashMap<>();
+    		for (String contest: new String[] {"A", "B", "C", "D", "E"}) {
+    			List<Map<String, Object>> contestSubmissions = repository.listContestSubmissions(city, contest);
+        		Map<String, List<Map<String, Object>>> usersSubmissions = contestSubmissions.stream().collect(Collectors.groupingBy(s->s.get("username").toString()));
+        		for (List<Map<String, Object>> list: usersSubmissions.values()) {
+        			while (list.size() < 3) list.add(new HashMap<>());
+        		}
+        		result.put(contest, usersSubmissions);
+    		}
+    		
+    		System.out.println(result);
+    		model.addAttribute("contests", result);
+        	return "result";
+        } else {
+        	model.addAttribute("city", city);
+        	return "upload";
+        }
+    }
 
     
     
