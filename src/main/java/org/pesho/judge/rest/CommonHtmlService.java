483917786ae698c6a633a1e100d08665d3b089e9
Petar Petrov <pppepito86@gmail.com>
2019-02-14 00:52:53 +0200

    disable unauth grade

--- src/main/java/org/pesho/judge/rest/CommonHtmlService.java
+++ src/main/java/org/pesho/judge/rest/CommonHtmlService.java
@@ -1,29 +1,9 @@
 package org.pesho.judge.rest;
 
-import java.io.File;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.io.FileUtils;
-import org.pesho.judge.util.HomographTranslator;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class CommonHtmlService extends HtmlService {
@@ -43,92 +23,92 @@ public class CommonHtmlService extends HtmlService {
     	return "/login";
     }
 
-	@GetMapping("/home")
-    public String table(@RequestParam(value = "city") Optional<String> maybeCity, Model model) {
-    	if (!maybeCity.isPresent()) {
-    		return "home";
-    	}
-    	
-    	String city = maybeCity.get().toLowerCase();
-    	if (city.isEmpty()) {
-    		return "home";
-    	}
-    	
-    	
-    	List<Map<String,Object>> submissions = repository.listCitySubmissions(city);
-    	if (submissions.size() > 0) {
-    		Map<String, Map<String, List<Map<String, Object>>>> result = new HashMap<>();
-    		for (String contest: new String[] {"A", "B", "C", "D", "E"}) {
-    			List<Map<String, Object>> contestSubmissions = repository.listContestSubmissions(city, contest);
-        		Map<String, List<Map<String, Object>>> usersSubmissions = contestSubmissions.stream().collect(Collectors.groupingBy(s->s.get("username").toString()));
-        		for (List<Map<String, Object>> list: usersSubmissions.values()) {
-        			boolean[] numbers = new boolean[4];
-        			for (Map<String, Object> s: list) numbers[Integer.valueOf(s.get("number").toString())]=true;
-        			for (int i = 1; i <= 3; i++) {
-        				if (numbers[i]) continue;
-        				HashMap<String, Object> m = new HashMap<>();
-        				m.put("number", i);
-        				list.add(m);
-        			}
-        			
-        			Collections.sort(list, new Comparator<Map<String, Object>>() {
-        				@Override
-        				public int compare(Map<String, Object> o1, Map<String, Object> o2) {
-        					return Integer.valueOf(o1.get("number").toString()) - Integer.valueOf(o2.get("number").toString());
-        				}
-					});
-        		}
-        		result.put(contest, usersSubmissions);
-    		}
-    		
-    		System.out.println(result);
-    		model.addAttribute("contests", result);
-        	return "result";
-        } else {
-        	model.addAttribute("city", city);
-        	return "upload";
-        }
-    }
+//	@GetMapping("/home")
+//    public String table(@RequestParam(value = "city") Optional<String> maybeCity, Model model) {
+//    	if (!maybeCity.isPresent()) {
+//    		return "home";
+//    	}
+//    	
+//    	String city = maybeCity.get().toLowerCase();
+//    	if (city.isEmpty()) {
+//    		return "home";
+//    	}
+//    	
+//    	
+//    	List<Map<String,Object>> submissions = repository.listCitySubmissions(city);
+//    	if (submissions.size() > 0) {
+//    		Map<String, Map<String, List<Map<String, Object>>>> result = new HashMap<>();
+//    		for (String contest: new String[] {"A", "B", "C", "D", "E"}) {
+//    			List<Map<String, Object>> contestSubmissions = repository.listContestSubmissions(city, contest);
+//        		Map<String, List<Map<String, Object>>> usersSubmissions = contestSubmissions.stream().collect(Collectors.groupingBy(s->s.get("username").toString()));
+//        		for (List<Map<String, Object>> list: usersSubmissions.values()) {
+//        			boolean[] numbers = new boolean[4];
+//        			for (Map<String, Object> s: list) numbers[Integer.valueOf(s.get("number").toString())]=true;
+//        			for (int i = 1; i <= 3; i++) {
+//        				if (numbers[i]) continue;
+//        				HashMap<String, Object> m = new HashMap<>();
+//        				m.put("number", i);
+//        				list.add(m);
+//        			}
+//        			
+//        			Collections.sort(list, new Comparator<Map<String, Object>>() {
+//        				@Override
+//        				public int compare(Map<String, Object> o1, Map<String, Object> o2) {
+//        					return Integer.valueOf(o1.get("number").toString()) - Integer.valueOf(o2.get("number").toString());
+//        				}
+//					});
+//        		}
+//        		result.put(contest, usersSubmissions);
+//    		}
+//    		
+//    		System.out.println(result);
+//    		model.addAttribute("contests", result);
+//        	return "result";
+//        } else {
+//        	model.addAttribute("city", city);
+//        	return "upload";
+//        }
+//    }
 
-    @PostMapping("/home/grade")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public synchronized String addSubmission(@RequestPart("file") MultipartFile file, 
-			@RequestParam("city") String city, Model model)
-			throws Exception {
-		File zipFile = getFile("temp", city, city + ".zip");
-		zipFile.getParentFile().mkdirs();
-		
-		FileUtils.copyInputStreamToFile(file.getInputStream(), zipFile);
-		File zipFolder = getFile("temp", city, city);
-		unzip(zipFile, zipFolder);
-
-		List<File> listSourceFiles = listSourceFiles(zipFolder);
-		for (File sourceFile: listSourceFiles) {
-			String username = sourceFile.getParentFile().getName();
-			String contest = sourceFile.getParentFile().getParentFile().getName();
-			
-			contest = new HomographTranslator().translate(contest);
-			
-			String problemName = sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf('.'));
-			problemName = new HomographTranslator().translate(problemName);
-			
-			String fileName = sourceFile.getName();
-			int submissionId = repository.addSubmission(city, username, contest, problemName, fileName);
-			if (submissionId != 0) {
-				File newFile = getFile("submissions", String.valueOf(submissionId), fileName);
-				newFile.getParentFile().mkdirs();
-				FileUtils.copyFile(sourceFile, newFile);
-			} else {
-				String details = String.format("%s_%s_%s_%s", city, contest, username, problemName);
-				repository.addLog("submission", "problem not found for " + details, "");
-			}
-		}
-		
-		FileUtils.deleteQuietly(zipFile);
-		FileUtils.deleteQuietly(zipFolder);
-		
-		return "redirect:/home?city="+city;
-	}
+//    @PostMapping("/home/grade")
+//	@Consumes(MediaType.MULTIPART_FORM_DATA)
+//	public synchronized String addSubmission(@RequestPart("file") MultipartFile file, 
+//			@RequestParam("city") String city, Model model)
+//			throws Exception {
+//		File zipFile = getFile("temp", city, city + ".zip");
+//		zipFile.getParentFile().mkdirs();
+//		
+//		FileUtils.copyInputStreamToFile(file.getInputStream(), zipFile);
+//		File zipFolder = getFile("temp", city, city);
+//		unzip(zipFile, zipFolder);
+//
+//		List<File> listSourceFiles = listSourceFiles(zipFolder);
+//		for (File sourceFile: listSourceFiles) {
+//			String username = sourceFile.getParentFile().getName();
+//			String contest = sourceFile.getParentFile().getParentFile().getName();
+//			
+//			contest = new HomographTranslator().translate(contest);
+//			
+//			String problemName = sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf('.'));
+//			problemName = new HomographTranslator().translate(problemName);
+//			
+//			String fileName = sourceFile.getName();
+//			int submissionId = repository.addSubmission(city, username, contest, problemName, fileName);
+//			if (submissionId != 0) {
+//				File newFile = getFile("submissions", String.valueOf(submissionId), fileName);
+//				newFile.getParentFile().mkdirs();
+//				FileUtils.copyFile(sourceFile, newFile);
+//			} else {
+//				String details = String.format("%s_%s_%s_%s", city, contest, username, problemName);
+//				repository.addLog("submission", "problem not found for " + details, "");
+//			}
+//		}
+//		
+//		FileUtils.deleteQuietly(zipFile);
+//		FileUtils.deleteQuietly(zipFolder);
+//		
+//		return "redirect:/home?city="+city;
+//	}
 
     
 }
