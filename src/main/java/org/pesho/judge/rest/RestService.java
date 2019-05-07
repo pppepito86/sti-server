f4a3dd1af09ce0f5e4b936ed97ffef97c372ef67
Petar Petrov <pppepito86@gmail.com>
2019-05-07 08:58:38 +0300

    show submission details

--- src/main/java/org/pesho/judge/rest/RestService.java
+++ src/main/java/org/pesho/judge/rest/RestService.java
@@ -2,11 +2,16 @@ package org.pesho.judge.rest;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TreeSet;
 
+import org.pesho.grader.SubmissionScore;
+import org.pesho.grader.step.StepResult;
+import org.pesho.grader.step.Verdict;
 import org.pesho.grader.task.TaskDetails;
 import org.pesho.grader.task.TaskParser;
 import org.pesho.judge.repositories.Repository;
@@ -19,6 +24,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 public class RestService {
 	
@@ -27,6 +34,106 @@ public class RestService {
 	
 	@Autowired
 	protected Repository repository;
+	
+	protected ObjectMapper mapper = new ObjectMapper();
+	
+	@RequestMapping("/me")
+	public ResponseEntity<?> me() {
+		return repository.getUserDetails(getUsername())
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@RequestMapping("/tasks")
+	public ResponseEntity<?> tasks() {
+		Optional<String> contest = getContest();
+		return contest.map(c -> repository.listContestTasks(c))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@RequestMapping("/tasks/{problemNumber}")
+	public ResponseEntity<?> taskDetails(@PathVariable int problemNumber) {
+		return repository.getContestTask(getUsername(), problemNumber).map(task -> {
+			TaskDetails details = getTaskDetails(task.get("contestId").toString(), task.get("number").toString());
+			task.put("time", details.getTime());
+			task.put("memory", details.getMemory());
+			return task;
+		}).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+	@RequestMapping("/tasks/{problemNumber}/solutions")
+	public ResponseEntity<?> taskSolutions(
+			@PathVariable int problemNumber) {
+		List<Map<String, Object>> submissions = taskSubmissions(problemNumber);
+		return ResponseEntity.ok(submissions);
+	}
+
+    @RequestMapping("/tasks/{problemNumber}/solutions/{solutionNumber}")
+    public ResponseEntity<?> taskSolution(
+    		@PathVariable int problemNumber,
+    		@PathVariable int solutionNumber) throws Exception {
+    	List<Map<String,Object>> submissions = taskSubmissions(problemNumber);
+    	Map<String, Object> submission = submissions.get(solutionNumber-1);
+    	
+		List<List<Integer>> groups = groups(Integer.valueOf(submission.get("problem_id").toString()));
+		submission.put("groups", groups);
+    	
+    	TreeSet<Integer> feedback = feedback(Integer.valueOf(submission.get("problem_id").toString()));
+		submission.put("verdict", fixVerdict(submission.get("verdict").toString(), feedback));
+		
+    	String details = submission.get("details").toString();
+		if (details != null && !details.isEmpty()) {
+			SubmissionScore score = mapper.readValue(details, SubmissionScore.class);
+			for(String key: score.getScoreSteps().keySet()) {
+				StepResult stepResult = score.getScoreSteps().get(key);
+				if (!key.startsWith("Test")) continue;
+				
+				int testId = Integer.valueOf(key.split("Test")[1]);
+				if (feedback.size() == 0 || feedback.contains(testId)) continue;
+						
+				stepResult.setExpectedOutput("");
+				stepResult.setOutput("");
+				stepResult.setReason("");
+				stepResult.setTime(null);
+				stepResult.setVerdict(Verdict.HIDDEN);
+			}
+
+			if (feedback.size() == 0) {
+				submission.put("score", Math.round(score.getScore()));
+			} else {
+				submission.put("score", "?");
+			}
+			submission.put("compile", score.getScoreSteps().get("Compile"));
+			score.getScoreSteps().remove("Compile");
+			submission.put("tests", score.getScoreSteps());
+			
+			LinkedHashMap<String, StepResult> steps = new LinkedHashMap<>();
+			if (groups.size() <= score.getScoreSteps().size()) {
+				for (int i = 0; i < groups.size(); i++) {
+					StepResult step = new StepResult();
+					boolean hidden = false;
+					boolean ok = true;
+					for (int t: groups.get(i)) {
+						Verdict v = score.getScoreSteps().get("Test"+t).getVerdict();
+						if (v == Verdict.HIDDEN) hidden = true;
+						if (v != Verdict.OK) ok = false;
+					}
+					if (hidden) step.setVerdict(Verdict.HIDDEN);
+					else if (ok) step.setVerdict(Verdict.OK);
+					steps.put("Group"+(i+1), step);
+					
+					for (int j = 0; j < groups.get(i).size(); j++) {
+						steps.put("Test"+groups.get(i).get(j), score.getScoreSteps().get("Test"+groups.get(i).get(j)));
+					}
+				}
+			}
+			submission.put("steps", steps);
+		}
+    	
+		return ResponseEntity.ok(submission);
+    }
 	
 	private String getUsername() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -45,39 +152,6 @@ public class RestService {
 				.map(user -> user.get("contest"))
 				.map(Object::toString);
 	}
-
-    @RequestMapping("/me")
-    public ResponseEntity<?> me() {
-    	return repository.getUserDetails(getUsername())
-    			.map(ResponseEntity::ok)
-    	       .orElse(ResponseEntity.notFound().build());
-    }
-
-    @RequestMapping("/tasks")
-    public ResponseEntity<?> tasks() {
-    	Optional<String> contest = getContest();
-    	return contest.map(c -> repository.listContestTasks(c))
-    		.map(ResponseEntity::ok)
-	       .orElse(ResponseEntity.notFound().build());
-    }
-
-    @RequestMapping("/tasks/{problemNumber}")
-    public ResponseEntity<?> taskDetails(@PathVariable int problemNumber) {
-    	return repository.getContestTask(getUsername(), problemNumber).map(task -> {
-    		TaskDetails details = getTaskDetails(task.get("contestId").toString(), task.get("number").toString());
-    		task.put("time", details.getTime());
-    		task.put("memory", details.getMemory());
-    		return task;
-    	}).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-        
-    }
-    
-    @RequestMapping("/tasks/{problemNumber}/solutions")
-    public ResponseEntity<?> taskSolutions(
-    		@PathVariable int problemNumber) {
-    	List<Map<String, Object>> submissions = taskSubmissions(problemNumber);
-    	return ResponseEntity.ok(submissions);
-    }
     
 	protected String fixVerdict(String verdict, TreeSet<Integer> feedback) {
 		if (feedback.size() == 0) return verdict;
@@ -89,6 +163,32 @@ public class RestService {
 			if (!feedback.contains(i)) split[i-1] = "?";
 		}
 		return String.join(",", split);
+	}
+	
+	private List<List<Integer>> groups(int problemId) throws Exception {
+		Map<String, Object> problem = repository.getProblem(problemId).get();
+		File problemDir = getFile("problem", problem.get("contest_id").toString(), problem.get("number").toString());
+
+		TaskParser parser = new TaskParser(problemDir);
+		TaskDetails details = TaskDetails.create(parser);
+		String groups = details.getGroups();
+		
+		List<List<Integer>> result = new ArrayList<>();
+		if (groups.isEmpty()) return result;
+		
+		String[] split = groups.split(",");
+		for (int i = 0; i < split.length; i++) {
+			String[] split2 = split[i].split("-");
+			int start = Integer.valueOf(split2[0]);
+			int end = Integer.valueOf(split2[1]);
+
+			List<Integer> list = new ArrayList<>();
+			for (int j = start; j <= end; j++) list.add(j);
+			
+			result.add(list);
+		}
+		
+		return result;
 	}
 	
 	private TreeSet<Integer> feedback(int problemId) throws Exception {
@@ -135,14 +235,5 @@ public class RestService {
     	});
 		return submissions;
 	}
-
-    @RequestMapping("/tasks/{problemNumber}/solutions/{solutionNumber}")
-    public ResponseEntity<?> taskSolution(
-    		@PathVariable int problemNumber,
-    		@PathVariable int solutionNumber) {
-    	List<Map<String,Object>> submissions = taskSubmissions(problemNumber);
-    	//TODO validate range
-    	return ResponseEntity.ok(submissions.get(solutionNumber-1));
-    }
     
 }
